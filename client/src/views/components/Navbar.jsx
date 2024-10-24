e8e55f0fc328de376b3417c439e0a53a985e6e49
Sarang Hadagali <sarang.hadagali@gmail.com>
2024-10-24 19:34:54 -0400

    Navbar

---
+++ client/src/views/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Navbar.css";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<div className="navbar-container">
+				<h1 className="navbar-logo">My Voice Assistant</h1>
+				<ul className="navbar-menu">
+					<li className="navbar-item">
+						<Link to="/" className="navbar-link">
+							Home
+						</Link>
+					</li>
+					<li className="navbar-item">
+						<Link to="/about" className="navbar-link">
+							About
+						</Link>
+					</li>
+					<li className="navbar-item">
+						<Link to="/contact" className="navbar-link">
+							Contact
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
