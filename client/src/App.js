70ad512c640238c3db423b1efab449d016593a9f
soumith <soumith@zobaze.com>
2024-10-30 13:23:30 -0400

    merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,32 @@ import { Journal } from "./views/journal";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainView from "./views/MainView";
 import Navbar from "./views/components/Navbar";
+import React from "react";
+
+import { LandingPage } from "./views/landingPage";
+import { VoiceRecorder } from "./views/voiceRecorder";
+
 
 export const App = () => {
 	return (
 		// code below displays login and sign up page, you can call it like so when you want to render it
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<MainView />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
-				<Route path="/journal" element={<Journal />} />
-			</Routes>
-		</Router>
+		<div>
+    
+		<h1>Welcome to CS555 Team 16</h1>
+		<VoiceRecorder />
+		<LandingPage />
+		<Login />
+		<Signup />
+  
+		  <Router>
+			  <Navbar />
+			  <Routes>
+				  <Route path="/" element={<MainView />} />
+				  <Route path="/login" element={<Login />} />
+				  <Route path="/signup" element={<Signup />} />
+				  <Route path="/journal" element={<Journal />} />
+			  </Routes>
+		  </Router>
+	  </div>
 	);
 };
