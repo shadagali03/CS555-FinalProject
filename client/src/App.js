b24cee845f637fb9c1c247a6461b425f785ba1bd
semphoenix <rodriguezs2021@gmail.com>
2024-10-30 19:06:03 -0700

    fixed pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,27 +9,19 @@ import Navbar from "./views/components/Navbar";
 import { LandingPage } from "./views/landingPage";
 import { VoiceRecorder } from "./views/voiceRecorder";
 
-
 export const App = () => {
-	return (
-		// code below displays login and sign up page, you can call it like so when you want to render it
-		<div>
-    
-		<h1>Welcome to CS555 Team 16</h1>
-		<VoiceRecorder />
-		<LandingPage />
-		<Login />
-		<Signup />
-  
-		  {/* <Router>
-			  <Navbar />
-			  <Routes>
-				  <Route path="/" element={<MainView />} />
-				  <Route path="/login" element={<Login />} />
-				  <Route path="/signup" element={<Signup />} />
-				  <Route path="/journal" element={<Journal />} />
-			  </Routes>
-		  </Router> */}
-	  </div>
-	);
+  return (
+    // code below displays login and sign up page, you can call it like so when you want to render it
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainView />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/journal" element={<Journal />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
